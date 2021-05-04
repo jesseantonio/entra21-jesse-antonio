38df85977378410a8f63de2cd761830dc46439c5
Guest 01 <guest01@alunos.sc.senac.br>
2021-05-04 14:09:36 -0300

    Organizando as pastas no repositório do curso de programação

---
+++ Aula 03/resto/tudo.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        //Constantes são valores imutáveis
+        // Declaração de uma constante
+        const PI = 3.1415
+        const ANO_NASCIMENTO = 2000   
+        PI = 2 // Erro
+    </script>
+
+</body>
+</body>
+</html>
