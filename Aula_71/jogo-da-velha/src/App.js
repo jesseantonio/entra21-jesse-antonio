f35fd18fa9cc6a40d2d57fc83173ba9f0499250d
Jessé Antônio <jesseantonio1321@gmail.com>
2021-08-31 17:29:05 -0300

    Jogo da Velha

---
+++ Aula_71/jogo-da-velha/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { Board } from './components/board';
+function App() {
+    return ( 
+        
+    <Board/>
+    
+    );
+}
+export default App;
+
---
+++ Aula_71/jogo-da-velha/src/components/square/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
+    render() {
+        return (
+            <button
+                className="square"
+                onClick={() => this.props.onClick()}
+            >
+                {this.props.value}
+            </button>
+        );
+    }
+}
